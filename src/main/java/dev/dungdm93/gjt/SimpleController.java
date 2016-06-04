36ae062abb983add5654033d53ea48779ec3a4d6
dungdm93 <dungdm93@live.com>
2016-06-04 10:31:50 +0700

    Demo GitLab merge request (MR), Jenkins auto build MR, auto deploy to Tomcat if success.

---
+++ src/main/java/dev/dungdm93/gjt/SimpleController.java
@@ -0,0 +1,15 @@
+package dev.dungdm93.gjt;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SimpleController {
+
+    @RequestMapping("/simple")
+    @ResponseBody
+    public String simple() {
+        return "Hello world!";
+    }
+}
---
+++ src/test/java/dev/dungdm93/gjt/SimpleControllerTest.java
@@ -0,0 +1,19 @@
+package dev.dungdm93.gjt;
+
+import org.junit.Test;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+public class SimpleControllerTest {
+    @Test
+    public void simple() throws Exception {
+        standaloneSetup(new SimpleController()).build()
+                .perform(get("/simple"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/plain;charset=ISO-8859-1"))
+                .andExpect(content().string("Hello world!"));
+    }
+}
