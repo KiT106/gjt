e9b97aff9db0eb950f74abb9ab14d70e77abc28c
dungdm93 <dungdm93@live.com>
2016-06-05 11:40:14 +0700

    add service

---
+++ src/main/java/dev/dungdm93/gjt/GjtService.java
@@ -0,0 +1,10 @@
+package dev.dungdm93.gjt;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GjtService {
+    public int boundedMultiply(int lhs, int rhs) {
+        return lhs + rhs;
+    }
+}
